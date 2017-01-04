e72454cb63a282d20217c5640f796ec9f186ddcb
Alessandro Resta <alessandro.resta@gmail.com>
2017-01-04 19:17:44 +0000

    Read server response and send result back to the caller
    Closes: #1

--- src/publisher/publisher.go
+++ src/publisher/publisher.go
@@ -8,16 +8,21 @@ import (
 
 	"fmt"
 
+	"io/ioutil"
+
 	"github.com/devopsgig/utilities/src/utilities"
 )
 
-// Meta hold the base fields with meta information for a publisher type
+const addr = "http://192.168.50.3"
+const port = "8080"
+
+// Meta hold the base fields with information for a publisher type
 type Meta struct {
 	UUID     string `json:"UUID"`
 	TaskType string `json:"task"`
 }
 
-// Task interface set the contract for types
+// Task interface set the contract for publisher types
 // which want to implement the publisher.Task interface
 type Task interface {
 	SetUUID(UUID string)
@@ -25,39 +30,53 @@ type Task interface {
 	GetTaskType() string
 }
 
-const addr = "http://192.168.50.3"
-const port = "8080"
-
 // Send function receives tasks of type publisher.Task,
 // send them to the respective endpoint in the REST API
 // and transmit the server response through the response channel
 func Send(task Task, respCh chan string) {
+
 	// Get new UUID
 	UUID, err := utilities.UUID()
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// Set UUID to task
 	task.SetUUID(UUID)
+
 	// Parse task to JSON format
 	taskJSON, err := json.Marshal(task)
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// Get the url endpoint for the task type
 	endpoint, err := endpoint(task.GetTaskType())
 	if err != nil {
 		log.Fatal(err)
 	}
+
 	// Form the URL
 	URL := fmt.Sprintf("%s:%s/%s", addr, port, endpoint)
+
 	// Send task in json format as POST request to REST API
 	data := url.Values{"task": []string{string(taskJSON)}}
+
+	// POST request
 	resp, err := http.PostForm(URL, data)
 	if err != nil {
 		log.Fatal(err)
 	}
-	respCh <- fmt.Sprintf("Response: %d", resp.StatusCode)
+
+	// Read the server response
+	result, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+
+	// Send the result back to the caller
+	respCh <- string(result)
 }
 
 // endpoint returns the endpoint for the taskType
